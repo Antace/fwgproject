a983909ec915b2e01ef8df823360a44293cc60b4
Antace <earthyon@hotmail.com>
2023-11-09 15:52:56 +0700

    Update salary_list_show.php

--- admin/salary_list_show.php
+++ admin/salary_list_show.php
@@ -1,4 +1,23 @@
+<script language="JavaScript">
+	function ClickCheckAll(vol)
+	{
+	
+		var a=1;
+		for(a=1;a<=document.frmsalary.hdnCount.value;a++)
+		{
+			if(vol.checked == true)
+			{
+				eval("document.frmsalary.checkbox"+a+".checked=true");
+			}
+			else
+			{
+				eval("document.frmsalary.checkbox"+a+".checked=false");
+			}
+		}
+	}
+</script>
 <?php 
+
   if(@$_GET['do']=='success'){
     echo '<script type="text/javascript">
           swal("", "ทำรายการสำเร็จ !!", "success");
@@ -52,50 +71,64 @@ $num2 = mysqli_num_rows($result);
 
 
 echo "ผลลัพธ์ = $num2";
+echo "<br>";
 //สร้างตัวแปร $row มารับค่าจากการ fetch array
 
 
 //วนลูป
-echo '<table id="example1" class="table table-bordered table-striped">';
-  echo "<thead>";
-    echo "<tr class=''>
-    <th width='5%'></th>
+?>
+
+<form name="frmsalary" action="salaryprintall.php" method="post">
+<table id="example1" class="table table-bordered table-striped">
+<input type = 'submit' class='btn btn-primary' name='btnprint' value='พิมพ์รายการที่เลือก'>
+  <thead>
+    <tr class=''>
+    <th width='5'> <div align='center'>
+      <input name='CheckAll' type='checkbox' id='CheckAll'  value='Y' onClick='ClickCheckAll(this);'>
+      </div></th>
       <th width='5%'>ID</th>
       <th width='20%'>ข้อมูลส่วนตัว</th>
       <th width='20%'>แผนก</th>
       <th width='30%'>ตำแหน่ง</th>
-      <th width='10%'>วันที่ทำรายการ</th>
-      <th width='7%'>-</th>
+      <th width='15%'>วันที่ทำรายการ</th>
+      <th width='10%'>-</th>
       
       
-    </tr>";
-  echo "</thead>";
+    </tr>
+  </thead>
+  <?php
   $i=1;
-  while($row = mysqli_fetch_array($result)) {
-  echo "<tr>";
-  
-  echo"<td><input type='checkbox' id='vehicle1' name='vehicle1' value='Bike'></td>";
-    echo "<td  class='hidden-xs'>" .  $i++   .  "</td> ";
-    // ."<br>"." password: ".sha1($row['a_password'])  
-    // ."<br>"."<a href='resetpass_admin.php?salary_id=".$row['salary_id']."' class='btn btn-primary btn-xs'>(เปลี่ยนรหัสผ่าน)</a></td> ";
-    echo "<td> ชื่อ ".$row["employee_name"]
-    // ."<br> เลขบัญชี ".$row["AccountNumber"]
-    ."</td> ";
-    echo "<td> ".$row["name_dept"].  "</td> ";
-      // echo "<td class='hidden-xs' align='center'>";
-  echo "<td> ".$row["name_position"].  "</td> ";
-  echo "<td> ".$row["salary_date"].  "</td> ";
-
-        
-    echo "<td><a href='salary.php?act=edit&ID=$row[salary_id]' class='btn btn-warning btn-xs'><i class='fas fa-pencil-alt'></i></a>   
-          <a href='salary_del_db.php?ID=$row[salary_id]' onclick=\"return confirm('ยันยันการลบ')\" class='btn btn-danger btn-xs'><i class='fas fa-trash'></i></a>
-          <a href='salary.php?act=print&ID=$row[salary_id]' class='btn btn-info btn-xs'><i class='fas fa-print'></i></a>
-    </td> ";
+  $a=0;
+  while($row = mysqli_fetch_assoc($result)) {
+  $a++;
+  ?>
+  <tr>
+ 
+  <td><input type='checkbox'  name='checkbox[]' id='checkbox<?php echo $a;?>' value='<?php echo $row['salary_id'];?>'></td>
+    <td  class='hidden-xs'>  <?php echo $i++  ; ?> </td> 
     
-  } 
-echo "</table>";
-mysqli_close($con);//ดูชื่อ ตัวแปรในไฟล์ connect ให้ดีว่า conหรือ condb หรืออย่างอื่น
-?>
-<?php 
+    <td> รหัส <?php echo $row["emp_id"];?>
+     <br> ชื่อ <?php echo $row["employee_name"];?>
+    </td>
+    <td> <?php echo $row["name_dept"];?>  </td>
+      
+  <td> <?php echo $row["name_position"];?>  </td>
+  <td> <?php echo $row["salary_date"];?>  </td>
 
- ?>
+   
+    <td><a href='salary.php?act=edit&ID=<?php echo $row["salary_id"];?>' class='btn btn-warning btn-xs'><i class='fas fa-pencil-alt'></i></a>   
+          <a href='salary_del_db.php?ID=<?php echo $row["salary_id"];?>' onclick=\'return confirm("ยันยันการลบ")\'' class='btn btn-danger btn-xs'><i class='fas fa-trash'></i></a>
+          <a href='salary.php?act=print&ID=<?php echo $row["salary_id"];?>' class='btn btn-info btn-xs'><i class='fas fa-print'></i></a>
+          
+    </td>
+    
+<?php  } ?>
+  
+</table>
+<?php
+mysqli_close($con);
+?>
+
+<input type='hidden' name='hdnCount' value='<?php echo $a; ?>'>
+</form>
+
