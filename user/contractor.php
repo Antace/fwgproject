bf9c9199357202a2e4ca88d3e32b22159e7de8d5
Antace <earthyon@hotmail.com>
2023-11-09 15:53:35 +0700

    Create contractor.php

---
+++ user/contractor.php
@@ -0,0 +1,58 @@
+<?php include('h.php');?>
+<body class="hold-transition skin-purple sidebar-mini">
+  <div class="wrapper">
+    <!-- Main Header -->
+    <?php include('menutop.php');?>
+    <!-- Left side column. contains the logo and sidebar -->
+    
+        <?php include('menu_l.php');?>
+      
+    <div class="content-wrapper">
+      <section class="content-header">
+      <h1>
+        <i class="glyphicon glyphicon-user hidden-xs"></i> <span class="hidden-xs">ข้อมูลผู้รับเหมา</span>
+        <a href="contractor.php?act=add" class="btn btn-primary btn-sm">เพิ่มข้อมูลผู้รับเหมา</a>
+        </h1>
+        
+      </section>
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="sticky-top mb-12">
+                <div class="card">
+                  <div class="card-body">
+                    <?php
+                  $act = (isset($_GET['act']) ? $_GET['act'] : '');
+                  if($act == 'add'){
+                  include('contractor_form_add.php');
+                  }elseif ($act == 'edit') {
+                  include('contractor_form_edit.php');
+                  }elseif($act=='view'){
+                  include('contractor_form_view.php');
+                  }else {
+                  include('contractor_list.php');
+                  }
+                  ?>                   
+                  <!-- /.card-body -->
+                  </div>
+                  <!-- /.card -->
+                </div>
+                <!-- /.col -->
+                
+              </div>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+          
+        </div>
+      </div>
+      
+    </div>
+    <!-- /.row -->
+    </div><!-- /.container-fluid -->
+      </section>
+    <?php include ('footer.php');?>
+  </html>
+  <?php include('footerjs.php');?>
