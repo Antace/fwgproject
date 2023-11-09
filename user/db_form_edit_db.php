4d1b5c045b2883ce28acf620861fa8a14c61563b
Antace <earthyon@hotmail.com>
2023-11-09 15:54:42 +0700

    Update db_form_edit_db.php

--- user/db_form_edit_db.php
+++ user/db_form_edit_db.php
@@ -14,31 +14,77 @@ if($_SESSION['employee_id']==''){
 	$db_name = mysqli_real_escape_string($con,$_POST["db_name"]);
 	$db_date = mysqli_real_escape_string($con,$_POST["db_date"]);
 	$db_file2 = mysqli_real_escape_string($con,$_POST["db_file2"]);
-	$date1 = date("Ymd_His");
-	$numrand = (mt_rand());
-	$db_file = (isset($_POST['db_file']) ? $_POST['db_file'] : '');
-	$upload=$_FILES['db_file']['name'];
-	if($upload !='') { 
 
+    $date1 = date("Ymd_His");
+	$numrand = (mt_rand());
+	$prodcut_img = (isset($_POST['db_file']) ? $_POST['db_file'] : '');
+	$upload=$_FILES['db_file']['name'];
+	$type = strrchr($_FILES['db_file']['name'],".");
+	
+	
+	if($type=='.pdf'){
+		if($upload !='') { 
 		$path="../db_file/";
 		$type = strrchr($_FILES['db_file']['name'],".");
-		$newname =$numrand.$date1.$type;
-		$path_copy=$path.$newname;
-		$path_link="../db_file/".$newname;
-		move_uploaded_file($_FILES['db_file']['tmp_name'],$path_copy);  
-	}else{
-		$newname=$db_file2;
-	}
+		$fileNewName =$numrand.$date1.$type;
+		$path_copy=$path.$fileNewName;
+		$path_link="../db_file/".$fileNewName;
+		move_uploaded_file($_FILES['db_file']['tmp_name'],$path_copy); 
+		}
+	}elseif(isset($_POST["submit"]) && !$_FILES['db_file']['error']) {
+        $file = $_FILES['db_file']['tmp_name']; 
+        $sourceProperties = getimagesize($file);
+		$type = strrchr($_FILES['db_file']['name'],".");
+        $fileNewName = time().$type;
+        $folderPath = "../db_file/";
+        $ext = pathinfo($_FILES['db_file']['name'], PATHINFO_EXTENSION);
+        $imageType = $sourceProperties[2];
 
+        switch ($imageType) {
+
+            case IMAGETYPE_PNG:
+                $imageResourceId = imagecreatefrompng($file); 
+                $targetLayer = imageResize($imageResourceId,$sourceProperties[0],$sourceProperties[1]);
+                imagepng($targetLayer,$folderPath. $fileNewName);
+                break;
+
+            case IMAGETYPE_GIF:
+                $imageResourceId = imagecreatefromgif($file); 
+                $targetLayer = imageResize($imageResourceId,$sourceProperties[0],$sourceProperties[1]);
+                imagegif($targetLayer,$folderPath. $fileNewName);
+                break;
+
+            case IMAGETYPE_JPEG:
+                $imageResourceId = imagecreatefromjpeg($file); 
+                $targetLayer = imageResize($imageResourceId,$sourceProperties[0],$sourceProperties[1]);
+                imagejpeg($targetLayer,$folderPath. $fileNewName);
+                break;
+
+            default:
+                echo "Invalid Image type.";
+                exit;
+                break;
+        }
+        // move_uploaded_file($file, $folderPath. $fileNewName. "_origin.". $ext);
+    } else {
+        $fileNewName=$db_file2;
+    }
+	function imageResize($imageResourceId,$width,$height) {
+        $targetWidth = $width < 1280 ? $width : 1280 ;
+        $targetHeight = ($height/$width)* $targetWidth;
+        $targetLayer = imagecreatetruecolor($targetWidth,$targetHeight);
+        imagecopyresampled($targetLayer, $imageResourceId, 0, 0, 0, 0, $targetWidth, $targetHeight, $width, $height);
+        return $targetLayer;
+    }
 	$sql = "UPDATE tb_po SET 
 	po_name='$po_name',
 	db_name='$db_name',
 	db_date='$db_date',
-	db_file='$newname'
+	db_file='$fileNewName'
 	WHERE po_id=$po_id
 	 ";
 
-	$result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error());
+	$result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error($con));
 	mysqli_close($con);
 	
 	if($result){
