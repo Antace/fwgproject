7e613ab2fae4649964317262083a46f9997281ab
Antace <earthyon@hotmail.com>
2023-11-09 15:54:02 +0700

    Update customer_form_add.php

--- user/customer_form_add.php
+++ user/customer_form_add.php
@@ -1,74 +1,86 @@
-<script type="text/javascript">
-        function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
 
-                reader.onload = function (e) {
-                    $('#blah').attr('src', e.target.result);
-                }
-                
-                reader.readAsDataURL(input.files[0]);
-            }
-        }
-</script>
-<?php 
- if(@$_GET['do']=='f'){
-            echo '<script type="text/javascript">
+<?php
+if (@$_GET['do'] == 'f') {
+  echo '<script type="text/javascript">
             swal("", "กรุณาใส่ข้อมูลให้ถูกต้อง !!", "warning");
             </script>';
-            echo '<meta http-equiv="refresh" content="2;url=customer.php?act=add" />';
- }elseif(@$_GET['do']=='d'){
-            echo '<script type="text/javascript">
+  echo '<meta http-equiv="refresh" content="2;url=customer.php?act=add" />';
+} elseif (@$_GET['do'] == 'd') {
+  echo '<script type="text/javascript">
             swal("", "ชื่อบริษัทซ้ำ กรุณาเปลี่ยน  !!", "error");
             </script>';
-            echo '<meta http-equiv="refresh" content="1;url=customer.php?act=add" />';
+  echo '<meta http-equiv="refresh" content="1;url=customer.php?act=add" />';
+}
 
- }
- ?>
 
+?>
+
+<div align="right">
+  <font color="red">*</font>
+  <font color="gray">Required Fields</font>
+</div>
+<hr>
 <form action="customer_form_add_db.php" method="post" class="form-horizontal" enctype="multipart/form-data">
   <div class="form-group">
-    <div class="col-sm-2 control-label">
-      ชื่อบริษัท :
+    <div class="col-sm-9 control-label">
+      ชื่อลูกค้า : <font color="red">*</font>
     </div>
     <div class="col-sm-3">
-      <input type="text" name="customer_name" required class="form-control" autocomplete="off"  minlength="2">
+      <input type="text" name="customer_name" required class="form-control" autocomplete="off" minlength="2">
     </div>
   </div>
   <div class="form-group">
-    <div class="col-sm-2 control-label" class="form-control" >
-      ที่อยู่ :
+    <div class="col-sm-3 control-label" class="form-control">
+      ที่อยู่ : <font color="red">*</font>
     </div>
-    <div class="col-sm-3">
-      <textarea name="customer_address" cols="60"></textarea>
+    <div class="col-sm-12">
+      <textarea name="customer_address" cols="60" required class="form-control"></textarea>
     </div>
   </div>
   <div class="form-group">
-    <div class="col-sm-2 control-label">
-      สาขา :
+    <div class="col-sm-3 control-label">
+      สาขา : <font color="red">*</font>
     </div>
     <div class="col-sm-3">
       <input type="text" name="customer_branch" required class="form-control">
     </div>
   </div>
   <div class="form-group">
-    <div class="col-sm-2 control-label">
-      เลขประจำตัวผู้เสียภาษี :
+    <div class="col-sm-3 control-label">
+      เลขประจำตัวผู้เสียภาษี : <font color="red">*</font>
     </div>
     <div class="col-sm-3">
-      <input type="text" name="customer_tax" required class="form-control">
+      <input name="customer_tax" type="text" onkeyup="autoTab(this)" size="17" required class="form-control">
     </div>
   </div>
   <div class="form-group">
-    <div class="col-sm-2 control-label">
-      เบอร์โทร :
+    <div class="col-sm-3 control-label">
+      ประเภท : <font color="red">*</font>
     </div>
-    <div class="col-sm-3">
-      <input type="text" name="customer_tel" required class="form-control">
+    <div class="col-sm-6">
+      <select class="select2" multiple="multiple" data-placeholder="ประเภท" name="customer_type" style="width: 100%;" required>
+        <option value="">ประเภท</option>
+        <option value="นิติบุคคล">นิติบุคคล</option>
+        <option value="บุคคลธรรมดา">บุคคลธรรมดา</option>
+      </select>
     </div>
   </div>
   <div class="form-group">
-    <div class="col-sm-2">
+    <div class="col-sm-3 control-label">
+      เครดิต : <font color="red">*</font>
+    </div>
+    <div class="col-sm-3">
+      <input type="number" name="customer_credit"  required class="form-control" value="0">
+    </div>
+  </div>
+  <hr>
+  <div class="form-group">
+      <div class="col-sm-6">
+        <input type="hidden" name="username" required class="form-control" value="<?php echo $username; ?>" readonly >
+      </div>
+    </div>
+  <div class="form-group">
+    <div class="col-sm-3">
     </div>
     <div class="col-sm-3">
       <button type="submit" class="btn btn-success">เพิ่มข้อมูล</button>
