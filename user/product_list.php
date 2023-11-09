32de4f1005dd6064e965d7b0cde768e893789c90
Antace <earthyon@hotmail.com>
2023-11-09 16:03:39 +0700

    Create product_list.php

---
+++ user/product_list.php
@@ -0,0 +1,44 @@
+<?php
+if (@$_GET['do'] == 'success') {
+  echo '<script type="text/javascript">
+        swal("", "ทำรายการสำเร็จ !!", "success");
+        </script>';
+  echo '<meta http-equiv="refresh" content="1;url=product.php" />';
+} else if (@$_GET['do'] == 'finish') {
+  echo '<script type="text/javascript">
+        swal("", "แก้ไขสำเร็จ !!", "success");
+        </script>';
+  echo '<meta http-equiv="refresh" content="1;url=product.php" />';
+}
+$query = "SELECT * FROM tb_product
+ORDER BY product_id DESC" or die("Error:" . mysqli_error());
+$result = mysqli_query($con, $query);
+echo ' <table id="example1" class="table table-bordered table-striped">';
+echo "<thead  align=center>";
+echo "<tr class='table-light'>
+      <th width='3%'>ลำดับ</th>
+      <th width='15%'>รหัสสินค้า</th>
+      <th width='30%'>ชื่อสินค้า</th>
+      <th width='15%'>ราคา/หน่วย</th>
+      <th width='15%'>คงเหลือ</th>
+      <th width='15%'>น้ำหนัก</th>
+      <th width='7%'>-</th>
+    </tr>";
+echo "</thead>";
+$i = 1;
+while ($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td align=center>" . $i++  . "</td> ";
+  echo "<td align=center>" . $row["product_idn"] . "</td> ";
+  echo "<td align=left>" . $row["product_name"] . "</td> ";
+  echo "<td align=right>" . number_format($row["product_price"],2) .  "</td> ";
+  echo "<td align=right>" . $row["product_uom"] . "&nbsp;". $row["product_unit"]. "</td> ";
+  echo "<td align=right>" . $row["product_weight"] ."&nbsp;". "kg"."</td> ";
+  
+
+  echo "<td align=center><a href='product.php?act=edit&ID=$row[product_id]' class='btn btn-warning btn-xs'><i class='fas fa-pencil-alt'></i></a>
+  <a href='product_del_db.php?ID=$row[product_id]' onclick=\"return confirm('ยันยันการลบ')\" class='btn btn-danger btn-xs'><i class='fas fa-trash'></i></a>
+</td> ";
+}
+echo "</table>";
+mysqli_close($con);
