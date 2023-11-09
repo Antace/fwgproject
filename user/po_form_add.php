8532519ae657e8bc3a12b2a2fc6db607a69c9e8a
Antace <earthyon@hotmail.com>
2023-11-09 16:01:12 +0700

    Update po_form_add.php

--- user/po_form_add.php
+++ user/po_form_add.php
@@ -3,20 +3,8 @@ $query2 = "SELECT * FROM tb_customer ORDER BY customer_id asc" or die("Error:" .
 $result2 = mysqli_query($con, $query2);
 $query3 = "SELECT * FROM tb_department ORDER BY department_id asc" or die("Error:" . mysqli_error());
 $result3 = mysqli_query($con, $query3);
-$query4 = "SELECT * FROM tb_category ORDER BY category_id asc" or die("Error:" . mysqli_error());
-$result4 = mysqli_query($con, $query4);
+
 ?>
-<script type="text/javascript">
-function readURL(input) {
-if (input.files && input.files[0]) {
-var reader = new FileReader();
-reader.onload = function (e) {
-$('#blah').attr('src', e.target.result);
-}
-reader.readAsDataURL(input.files[0]);
-}
-}
-</script>
 <?php
 if(@$_GET['do']=='f'){
 echo '<script type="text/javascript">
@@ -30,11 +18,16 @@ swal("", "เลขใบสั่งซื้อซ้ำ กรุณาเป
 echo '<meta http-equiv="refresh" content="1;url=po.php?act=add" />';
 }
 ?>
+<div align="right">
+  <font color="red">*</font>
+  <font color="gray">Required Fields</font>
+</div>
+<hr>
 <form action="po_form_add_db.php" method="post" class="form-horizontal" enctype="multipart/form-data">
   <div class="form-group">
     <div class="form-group">
       <div class="col-sm-2 control-label">
-        เลขที่ PO :
+        เลขที่ PO : <font color="red">*</font>
       </div>
       <div class="col-sm-6">
         <input type="text" name="po_name" required class="form-control">
@@ -42,7 +35,7 @@ echo '<meta http-equiv="refresh" content="1;url=po.php?act=add" />';
     </div>
     <div class="form-group">
       <div class="col-sm-2 control-label">
-        วันที่ :
+        วันที่ : <font color="red">*</font>
       </div>
       <div class="col-sm-2">
         <input type="date" name="po_date" required class="form-control">
@@ -50,47 +43,39 @@ echo '<meta http-equiv="refresh" content="1;url=po.php?act=add" />';
     </div>
     <div class="col-sm-6">
       <div class="form-group">
-        บริษัท:
-        <select class="select2" multiple="multiple" data-placeholder="บริษัท" name ="customer_name" style="width: 100%;" required>
+        บริษัท : <font color="red">*</font>
+        <select class="select2bs4"  data-placeholder="บริษัท" name ="customer_name" style="width: 100%;" required>
+        <option value="">-</option>
           <?php foreach($result2 as $results){?>
           <option value="<?php echo $results["customer_name"];?>">
             <?php echo $results["customer_name"]; ?>
           </option>
           <?php } ?>
-        </select>
+        </select><a href="customer.php?act=add" ">เพิ่มบริษัท</a>
       </div>
     </div>
     <div class="col-sm-6">
       <div class="form-group">
-        โครงการ:
-        <select class="select2" multiple="multiple" data-placeholder="โครงการ" name ="department_name" style="width: 100%;" required>
+        โครงการ : <font color="red">*</font>
+        <select class="select2bs4"  data-placeholder="โครงการ" name ="department_name" style="width: 100%;" required>
+        <option value="">-</option>
           <?php foreach($result3 as $results){?>
           <option value="<?php echo $results["department_name"];?>">
             <?php echo $results["department_name"]; ?>
           </option>
           <?php } ?>
-        </select>
+        </select><a href="department.php?act=add" ">เพิ่มโครงการ</a>
       </div>
     </div>
-    <div class="col-sm-6">
-      <div class="form-group">
-        ประเภทงาน:
-        <select class="select2" multiple="multiple" data-placeholder="ประเภทงาน" name ="category_name" style="width: 100%;"  required>
-          <?php foreach($result4 as $results){?>
-          <option value="<?php echo $results["category_name"];?>">
-            <?php echo $results["category_name"]; ?>
-          </option>
-          <?php } ?>
-        </select>
-      </div>
-    </div>
+    
     <div class="form-group">
       <div class="col-sm-2 control-label">
-        งานของ :
+        งานของ : <font color="red">*</font>
       </div>
       <div class="col-sm-6">
-        <select class="select2" multiple="multiple" data-placeholder="งานของ" name ="work_by" style="width: 100%;"  required>
-          <option value="">งานของ</option>
+        <select class="select2bs4"  data-placeholder="งานของ" name ="work_by" style="width: 100%;"  required>
+        <option value="">-</option>
+          <option value="ไม่ระบุ">ไม่ระบุ</option>
           <option value="LH">LH</option>
           <option value="SANSIRI">SANSIRI</option>
           <option value="QHOUSE">QHOUSE</option>
@@ -99,7 +84,7 @@ echo '<meta http-equiv="refresh" content="1;url=po.php?act=add" />';
     </div>
     <div class="form-group">
       <div class="col-sm-2 control-label">
-        แปลง :
+        แปลง : <font color="red">*</font>
       </div>
       <div class="col-sm-6">
         <input type="text" name="po_place" required class="form-control">
@@ -107,36 +92,64 @@ echo '<meta http-equiv="refresh" content="1;url=po.php?act=add" />';
     </div>
     <div class="form-group">
       <div class="col-sm-2 control-label">
-        ราคา :
+        ราคา : <font color="red">*</font>
       </div>
       <div class="col-sm-6">
-        <input type="number" name="po_price" required class="form-control">
+        <input type="decimal"  name="po_price" required class="form-control">
       </div>
     </div>
     <div class="form-group">
       <div class="col-sm-2 control-label">
-        ไฟล์ :
+        ไฟล์ : <font color="red">*</font>
       </div>
-      <div class="col-sm-6">
-        <font color="red">*อัพโหลดได้เฉพาะ .pdf เท่านั้น </font>
-        <input type="file" name="po_file"    class="form-control" accept="application/pdf" required>
+      <div class="custom-file col-sm-6">
+        <font color="red">*อัพโหลดได้เฉพาะไฟล์รูปและPDFเท่านั้น </font>
+        <input type="file" class="form-control" id="file" name="po_file" accept="image/application/PDF*" onchange="readURL(this)" required>
+        
       </div>
     </div>
-    <div class="form-group">
+    <!-- <div class="form-group">
       <div class="col-sm-2 control-label">
-        ผู้บันทึก :
+        รูป :
       </div>
+      <div class="col-sm-3">
+      <div id="imgControl" class="d-none">
+        <img id="imgUpload" class="img-fluid my-3">
+      </div>
+      </div>
+    </div> -->
+    
+    <hr>
+    <div class="form-group">
       <div class="col-sm-6">
-        <input type="text" name="username" required class="form-control" value="<?php echo $username; ?>" readonly >
+        <input type="hidden" name="username" required class="form-control" value="<?php echo $username; ?>" readonly >
       </div>
     </div>
+
+
+
+    
     <div class="form-group">
       <div class="col-sm-2">
       </div>
       <div class="col-sm-3">
-        <button type="submit" class="btn btn-success">เพิ่มข้อมูล</button>
+        <button type="submit" name="submit" class="btn btn-success">เพิ่มข้อมูล</button>
         <a href="po.php" class="btn btn-danger">ยกเลิก</a>
       </div>
     </div>
   </div>
 </form>
+
+<!-- <script>
+        function readURL(input){
+            if(input.files[0]){
+                let reader = new FileReader();
+                document.querySelector('#imgControl').classList.replace("d-none", "d-block");
+                reader.onload = function (e) {
+                    let element = document.querySelector('#imgUpload');
+                    element.setAttribute("src", e.target.result);
+                }  
+                reader.readAsDataURL(input.files[0]);
+            }         
+        }
+    </script> -->
