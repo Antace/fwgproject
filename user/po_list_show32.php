0c92f8b4fc4ac37ebe18d47196759eb13d859e23
Antace <earthyon@hotmail.com>
2023-11-09 16:01:57 +0700

    Update po_list_show32.php

--- user/po_list_show32.php
+++ user/po_list_show32.php
@@ -1,55 +1,8 @@
 <?php
-if(@$_GET['do']=='success'){
-echo '<script type="text/javascript">
-swal("", "ทำรายการสำเร็จ !!", "success");
-</script>';
-echo '<meta http-equiv="refresh" content="1;url=po.php" />';
-}else if(@$_GET['do']=='finish'){
-echo '<script type="text/javascript">
-swal("", "แก้ไขสำเร็จ !!", "success");
-</script>';
-echo '<meta http-equiv="refresh" content="1;url=po.php" />';
-}
-echo ' <table id="example1" class="table table-bordered table-striped">';
-  echo "<thead>";
-    //echo mysqli_num_rows($result1);
-    //exit;
-    echo "<tr class=''>
-      <th width='3%'  class='hidden-xs'>ID</th>
-      <th width='25%'>ข้อมูลใบสั่งซื้อ</th>
-      <th width='9%' class='hidden-xs'>PO</th>
-      <th width='2%'>*</th>
-      <th width='9%' class='hidden-xs'>DB</th>
-      <th width='2%'>*</th>
-      <th width='9%' class='hidden-xs'>IV</th>
-      <th width='2%'>*</th>
-      <th width='9%' class='hidden-xs'>PM</th>
-      <th width='2%'>*</th>
-      <th width='25%' class='hidden-xs'>ข้อมูลงานประกัน</th>
-      <th width='2%'>*</th>
-    </tr>";
-  echo "</thead>";
-  
-  echo 'เลขที่ใบสั่งซื้อ  = ';
-  echo '<font color="blue">'; 
-  echo $_GET['qpo'];
-  echo '</font>';
-  echo ' ลูกค้า  = ';
-  echo '<font color="blue">'; 
-  echo $_GET['qcus'];
-  echo '</font>';
-  echo ' โครงการ  = ';
-  echo '<font color="blue">'; 
-  echo $_GET['qdept'];
-  echo '</font>';
-  echo ' ประเภทงาน  = ';
-  echo '<font color="blue">'; 
-  echo $_GET['qcate'];
-  echo '</font>';
-  echo '<br/>'; 
+include "po_list_show_h.php";
   
   $query = "SELECT * FROM tb_po 
-  WHERE po_name LIKE '%$qpo%'  AND category_name LIKE '%$qcate%'  "  or die("Error:" . mysqli_error());
+  WHERE po_name LIKE '%$qpo%'  AND work_by LIKE '%$qcate%'  "  or die("Error:" . mysqli_error());
   $result = mysqli_query($con, $query);
  include "po_slist.php";
 
