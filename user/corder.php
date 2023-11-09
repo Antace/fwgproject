4f2c3e6021741a04bc6106a57c69dedf6f1eb007
Antace <earthyon@hotmail.com>
2023-11-09 15:53:55 +0700

    Create corder.php

---
+++ user/corder.php
@@ -0,0 +1,61 @@
+<?php include('h.php');?>
+<body class="hold-transition skin-purple sidebar-mini">
+  <div class="wrapper">
+    <!-- Main Header -->
+    <?php include('menutop.php');?>
+        <?php include('menu_l.php');?>
+    <div class="content-wrapper">
+      <section class="content-header">
+        <h1>
+        <i class="glyphicon glyphicon-check hidden-xs"></i> <span class="hidden-xs">ข้อมูลการสั่งป้ายที่ถูกยกเลิก</span> 
+        
+        </h1>
+      </section>
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="sticky-top mb-12">
+                <div class="card">
+                  <div class="card-body">
+                    <?php
+                     $act = (isset($_GET['act']) ? $_GET['act'] : '');
+                     if($act == 'view'){
+                     include('corder_form_view.php');
+                     }elseif($act == 'corder_cancel'){
+                      include('corder_form_view.php');
+                     }elseif($act=='print'){
+                      include('corder_form_print.php');
+                      }elseif($act=='printall'){
+                        include('corder_print.php');
+                      }else{
+                     include('corder_list.php');  
+                     
+                     }
+                       
+                  ?>         
+                 <!-- /.card-body -->
+                  </div>
+                  <!-- /.card -->
+                </div>
+                <!-- /.col -->
+                
+              </div>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+          
+        </div>
+      </div>
+      
+    </div>
+    <!-- /.row -->
+    </div><!-- /.container-fluid -->
+      </section>
+    <?php include ('footer.php');?>
+  </html>
+  <?php include('footerjs.php');?>
+
+
+
