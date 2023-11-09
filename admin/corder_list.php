2756c08f214af765dea53b6904926126ec2ebaed
Antace <earthyon@hotmail.com>
2023-11-09 14:50:53 +0700

    Update corder_list.php

--- admin/corder_list.php
+++ admin/corder_list.php
@@ -1,21 +1,3 @@
-<script language="JavaScript">
-	function ClickCheckAll(vol)
-	{
-	
-		var a=1;
-		for(a=1;a<=document.frmorder.hdnCount.value;a++)
-		{
-			if(vol.checked == true)
-			{
-				eval("document.frmorder.checkbox"+a+".checked=true");
-			}
-			else
-			{
-				eval("document.frmorder.checkbox"+a+".checked=false");
-			}
-		}
-	}
-</script>
 <?php
 //connect db
 if(@$_GET['do']=='success'){
@@ -31,19 +13,17 @@ if(@$_GET['do']=='success'){
   echo '<meta http-equiv="refresh" content="1;url=order.php" />';
 }
 $query = "SELECT * FROM tb_cancelorder
-ORDER BY corder_id DESC" or die("Error:" . mysqli_error());
+ORDER BY corder_id DESC" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
 ?>
 
 
 <form name="frmorder" action="orderprintall.php" method="post">
 <table id="example1" class="table table-bordered table-striped">
-<input type = 'submit' class='btn btn-primary  ' name='btnprint' value='พิมพ์รายการที่เลือก'>
+
 <thead>
   <tr class=''>
-  <th width='5'> <div align='center'>
-      <input name='CheckAll' type='checkbox' id='CheckAll'  value='Y' onClick='ClickCheckAll(this);'>
-      </div></th>
+  
     <th width='3%'>ลำดับ</th>
      <th width='20%'>เลขที่ใบสั่งผลิต</th>
      <th width='30%' >วันที่ทำรายการ</th>
@@ -61,7 +41,6 @@ $result = mysqli_query($con, $query);
   $a++;
   ?>
 <tr>
-<td><input type='checkbox'  name='checkbox[]' id='checkbox<?php echo $a;?>' value='<?php echo $row['order_id'];?>'></td>
   <td  align=center><?php echo  $i++ ; ?></td> 
   <td> <?php echo $row["corder_id"]; ?> </td> 
   <td> <?php echo $row["corder_date"]; ?></td> 
@@ -69,8 +48,8 @@ $result = mysqli_query($con, $query);
   
   
 <td align=center><a href='corder.php?act=view&ID=<?php echo $row["corder_id"];?>' class='btn btn-warning btn-xs'>เปิด</a>
-  <a href='corder.php?act=order_cancel&ID=<?php echo $row["corder_id"];?>' class='btn btn-danger btn-xs'>ลบ</a>
-  <a href='corder.php?act=print&ID=<?php echo $row["corder_id"];?>' class='btn btn-info btn-xs'><i class='fas fa-print'></i></a>
+  
+  
 </td>
   </tr>
 <?php } ?>
