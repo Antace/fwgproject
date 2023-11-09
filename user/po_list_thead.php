74331c092a13c5f911f51d9ec831dbc33699febb
Antace <earthyon@hotmail.com>
2023-11-09 16:02:34 +0700

    Create po_list_thead.php

---
+++ user/po_list_thead.php
@@ -0,0 +1,20 @@
+<?php
+echo ' <table id="example1" class="table table-bordered table-striped">';
+echo "<thead align='center'>";
+  //echo mysqli_num_rows($result1);
+  //exit;
+  echo "<tr align='center' class='table-light'>
+    <th width='3%'  >ลำดับ</th>
+    <th width='10%'>เลขที่ PO</th>
+    <th width='25%'>บริษัท</th>
+    <th width='20%'>โครงการ</th>
+    <th width='10%'>งานของ</th>
+    <th width='9%'>PO</th>
+    <th width='5%'>ราคา</th>
+    <th width='7%'>DB</th>
+    <th width='7%'>IV</th>
+    <th width='7%'>PM</th>
+    <th width='7%'>งานประกัน</th>
+  
+  </tr>";
+echo "</thead>";
