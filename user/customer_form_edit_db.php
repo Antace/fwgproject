2ff6ec6850116674c79a8a5c85e6a6d23ce9cd49
Antace <earthyon@hotmail.com>
2023-11-09 15:54:34 +0700

    Update customer_form_edit_db.php

--- user/customer_form_edit_db.php
+++ user/customer_form_edit_db.php
@@ -15,19 +15,22 @@ if($_SESSION['employee_id']==''){
 	$customer_address = mysqli_real_escape_string($con,$_POST["customer_address"]);
 	$customer_branch = mysqli_real_escape_string($con,$_POST["customer_branch"]);
 	$customer_tax = mysqli_real_escape_string($con,$_POST["customer_tax"]);
-	$customer_tel = mysqli_real_escape_string($con,$_POST["customer_tel"]);		
-	
+	$customer_type = mysqli_real_escape_string($con,$_POST["customer_type"]);	
+	$customer_credit = mysqli_real_escape_string($con,$_POST["customer_credit"]);	
+	$username = mysqli_real_escape_string($con,$_POST["username"]);
 
 	$sql = "UPDATE tb_customer SET 
 	customer_name='$customer_name',
 	customer_address='$customer_address',
 	customer_branch='$customer_branch',
 	customer_tax='$customer_tax',
-	customer_tel='$customer_tel'
+	customer_type='$customer_type',
+	customer_credit='$customer_credit',
+	username='$username'
 	WHERE customer_id=$customer_id
 	 ";
 
-	$result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error());
+	$result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error($con));
 	mysqli_close($con);
 	
 	if($result){
