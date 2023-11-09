6c292819770efbfb03e401ee2f0e913ff1b67095
Antace <earthyon@hotmail.com>
2023-11-09 16:01:08 +0700

    Update pm_form_edit_db.php

--- user/pm_form_edit_db.php
+++ user/pm_form_edit_db.php
@@ -34,19 +34,65 @@ if($_SESSION['employee_id']==''){
 	//exit;
 	$date1 = date("Ymd_His");
 	$numrand = (mt_rand());
-	$pm_file = (isset($_POST['pm_file2']) ? $_POST['pm_file2'] : '');
+	$prodcut_img = (isset($_POST['pm_file']) ? $_POST['pm_file'] : '');
 	$upload=$_FILES['pm_file']['name'];
-	if($upload !='') { 
-		//move_uploaded_file($_FILES['file']['tmp_name'],'/image/'.$upload);
+	$type = strrchr($_FILES['pm_file']['name'],".");
+	
+	
+	if($type=='.pdf'){
+		if($upload !='') { 
 		$path="../pm_file/";
 		$type = strrchr($_FILES['pm_file']['name'],".");
-		$newname =$numrand.$date1.$type;
-		$path_copy=$path.$newname;
-		$path_link="../pm_file/".$newname;
-		move_uploaded_file($_FILES['pm_file']['tmp_name'],$path_copy);  
-	}else{
-		$newname=$pm_file2;
-	}
+		$fileNewName =$numrand.$date1.$type;
+		$path_copy=$path.$fileNewName;
+		$path_link="../pm_file/".$fileNewName;
+		move_uploaded_file($_FILES['pm_file']['tmp_name'],$path_copy); 
+		}
+	}elseif(isset($_POST["submit"]) && !$_FILES['pm_file']['error']) {
+        $file = $_FILES['pm_file']['tmp_name']; 
+        $sourceProperties = getimagesize($file);
+		$type = strrchr($_FILES['pm_file']['name'],".");
+        $fileNewName = time().$type;
+        $folderPath = "../pm_file/";
+        $ext = pathinfo($_FILES['pm_file']['name'], PATHINFO_EXTENSION);
+        $imageType = $sourceProperties[2];
+
+        switch ($imageType) {
+
+            case IMAGETYPE_PNG:
+                $imageResourceId = imagecreatefrompng($file); 
+                $targetLayer = imageResize($imageResourceId,$sourceProperties[0],$sourceProperties[1]);
+                imagepng($targetLayer,$folderPath. $fileNewName);
+                break;
+
+            case IMAGETYPE_GIF:
+                $imageResourceId = imagecreatefromgif($file); 
+                $targetLayer = imageResize($imageResourceId,$sourceProperties[0],$sourceProperties[1]);
+                imagegif($targetLayer,$folderPath. $fileNewName);
+                break;
+
+            case IMAGETYPE_JPEG:
+                $imageResourceId = imagecreatefromjpeg($file); 
+                $targetLayer = imageResize($imageResourceId,$sourceProperties[0],$sourceProperties[1]);
+                imagejpeg($targetLayer,$folderPath. $fileNewName);
+                break;
+
+            default:
+                echo "Invalid Image type.";
+                exit;
+                break;
+        }
+        // move_uploaded_file($file, $folderPath. $fileNewName. "_origin.". $ext);
+    } else {
+        $fileNewName=$pm_file2;
+    }
+	function imageResize($imageResourceId,$width,$height) {
+        $targetWidth = $width < 1280 ? $width : 1280 ;
+        $targetHeight = ($height/$width)* $targetWidth;
+        $targetLayer = imagecreatetruecolor($targetWidth,$targetHeight);
+        imagecopyresampled($targetLayer, $imageResourceId, 0, 0, 0, 0, $targetWidth, $targetHeight, $width, $height);
+        return $targetLayer;
+    }
 	//print_r ($pm_file);
 	//echo "<br>";
 	//print_r ($_POST);
@@ -56,7 +102,7 @@ if($_SESSION['employee_id']==''){
 	po_name='$po_name',
 	pm_name='$pm_name',
 	pm_date='$pm_date',
-	pm_file='$newname',
+	pm_file='$fileNewName',
 	po_datesend='$po_datesend',
 	po_insurance='$po_insurance',
 	po_dateexpire='$po_dateexpire'
