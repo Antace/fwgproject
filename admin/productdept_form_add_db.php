a0aebce77f82dd9b6b63fbf308fef6a1bac1a9cb
Antace <earthyon@hotmail.com>
2023-11-09 17:02:18 +0700

    Update productdept_form_add_db.php

--- admin/productdept_form_add_db.php
+++ admin/productdept_form_add_db.php
@@ -43,7 +43,7 @@ if($_SESSION['employee_id']==''){
 	'$username'
 	)";
 
-	$result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error());
+	$result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error($con));
 }
 	mysqli_close($con);
 
