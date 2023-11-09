15f6366d0dd88a236772073f06e839a536056a95
Antace <earthyon@hotmail.com>
2023-11-09 14:50:39 +0700

    Update contractor_form_edit.php

--- admin/contractor_form_edit.php
+++ admin/contractor_form_edit.php
@@ -1,7 +1,7 @@
 <?php
 $ID = mysqli_real_escape_string($con, $_GET['ID']);
 $sql = "SELECT * FROM tb_contractor WHERE contractor_id=$ID";
-$result = mysqli_query($con, $sql) or die("Error in query: $sql " . mysqli_error());
+$result = mysqli_query($con, $sql) or die("Error in query: $sql " . mysqli_error($con));
 $row = mysqli_fetch_array($result);
 ?>
 
