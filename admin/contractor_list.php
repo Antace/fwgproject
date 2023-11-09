47481ca04cfb764d850e9a41bf4c9e6adde3ebb2
Antace <earthyon@hotmail.com>
2023-11-09 14:50:46 +0700

    Update contractor_list.php

--- admin/contractor_list.php
+++ admin/contractor_list.php
@@ -11,7 +11,7 @@ if (@$_GET['do'] == 'success') {
   echo '<meta http-equiv="refresh" content="1;url=contractor.php" />';
 }
 $query = "SELECT * FROM tb_contractor
-ORDER BY contractor_id DESC" or die("Error:" . mysqli_error());
+ORDER BY contractor_id DESC" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
 echo ' <table id="example1" class="table table-bordered table-striped">';
 echo "<thead  align=center>";
