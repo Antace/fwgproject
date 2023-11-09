eb09a10ae9a65f18239e252929dae5b37c56f198
Antace <earthyon@hotmail.com>
2023-11-09 14:51:00 +0700

    Update customer_form_add_db.php

--- admin/customer_form_add_db.php
+++ admin/customer_form_add_db.php
@@ -26,7 +26,7 @@ if($_SESSION['employee_id']==''){
 	FROM tb_customer
 	WHERE customer_name = '$customer_name'
 	";
-    $result1 = mysqli_query($con, $check) or die(mysqli_error());
+    $result1 = mysqli_query($con, $check) or die(mysqli_error($con));
     $num=mysqli_num_rows($result1);
 
     if($num > 0)
@@ -57,7 +57,7 @@ if($_SESSION['employee_id']==''){
 	'$username'
 	)";
 
-	$result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error());
+	$result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error($con));
 
 	}
 	mysqli_close($con);
