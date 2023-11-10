7ca104d8db9f23a86e78c438a09145bb11bd291b
Antace <earthyon@hotmail.com>
2023-11-10 10:02:49 +0700

    Update productdept.php

--- admin/productdept.php
+++ admin/productdept.php
@@ -1,15 +1,16 @@
-<?php include('h.php');?>
+<?php include('h.php'); ?>
+
 <body class="hold-transition skin-purple sidebar-mini">
   <div class="wrapper">
     <!-- Main Header -->
-    <?php include('menutop.php');?>
-        <?php include('menu_l.php');?>
+    <?php include('menutop.php'); ?>
+    <?php include('menu_l.php'); ?>
     <div class="content-wrapper">
       <section class="content-header">
         <h1>
-        <i class="glyphicon glyphicon-check hidden-xs"></i> <span class="hidden-xs">ข้อมูลสินค้าโครงการ</span> 
-        <a href="productdept.php?act=add" class="btn btn-primary btn-sm">เพิ่ม</a>
-        
+          <i class="glyphicon glyphicon-check hidden-xs"></i> <span class="hidden-xs">ข้อมูลสินค้าโครงการ</span>
+          <a href="productdept.php?act=add" class="btn btn-primary btn-sm">เพิ่ม</a>
+
         </h1>
       </section>
       <section class="content">
@@ -21,38 +22,38 @@
                   <div class="card-body">
                     <?php
                     $act = (isset($_GET['act']) ? $_GET['act'] : '');
-                    if($act == 'add'){
-                        include('productdept_form_add.php');
-                    }elseif ($act == 'productadd') {
+                    if ($act == 'add') {
                       include('productdept_form_add.php');
-                  }elseif ($act == 'edit') {
-                        include('productdept_form_edit.php');
-                    }else {
-                        include('productdept_list.php');
+                    } elseif ($act == 'productadd') {
+                      include('productdept_form_add.php');
+                    } elseif ($act == 'productedit') {
+                      include('productdept_form_add.php');
+                    } elseif ($act == 'edit') {
+                      include('productdept_form_edit.php');
+                    } else {
+                      include('productdept_list.php');
                     }
-                  ?>                   
-                 <!-- /.card-body -->
+                    ?>
+                    <!-- /.card-body -->
                   </div>
                   <!-- /.card -->
                 </div>
                 <!-- /.col -->
-                
+
               </div>
             </div>
             <!-- /.card-body -->
           </div>
           <!-- /.card -->
-          
+
         </div>
-      </div>
-      
     </div>
-    <!-- /.row -->
-    </div><!-- /.container-fluid -->
-      </section>
-    <?php include ('footer.php');?>
+
+  </div>
+  <!-- /.row -->
+  </div><!-- /.container-fluid -->
+  </section>
+  <?php include('footer.php'); ?>
+
   </html>
-  <?php include('footerjs.php');?>
-
-
-
+  <?php include('footerjs.php'); ?>
