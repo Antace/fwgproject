b4307313dbafbf6bbaa2a7d483f6e6f874b30e50
Antace <earthyon@hotmail.com>
2023-11-15 17:00:42 +0700

    Update reserve_product_list.php

--- admin/reserve_product_list.php
+++ admin/reserve_product_list.php
@@ -11,38 +11,51 @@
           </script>';
     echo '<meta http-equiv="refresh" content="1;url=reserve.php" />';
   }
-  $query = "SELECT * FROM tb_reserve
-ORDER BY reserve_id DESC" or die("Error:" . mysqli_error());
+  if($_GET['act']=='seeall'){
+    $query = "SELECT * FROM tb_reserve
+ORDER BY reserve_date DESC " or die("Error:" . mysqli_error($con));
   $result = mysqli_query($con, $query);
-
+  }else{
+  $query = "SELECT * FROM tb_reserve
+  WHERE reserve_date <= current_date
+ORDER BY reserve_date DESC " or die("Error:" . mysqli_error($con));
+  $result = mysqli_query($con, $query);
+  }
   echo ' <table id="example1" class="table table-bordered table-striped">';
   echo "<thead  align=center>";
   echo "<tr class=''>
       <th width='3%'>ลำดับ</th>
       <th width='7%'>เลขที่</th>
-      <th width='7%'>วันที่ขาย</th>
-      <th width='30%'>ชื่อลูกค้า</th>
+      <th width='7%'>วันที่นัดรับ</th>
+      <th width='20%'>ชื่อลูกค้า</th>
       <th width='10%'>สถานะรับของ</th>
-      <th width='10%'>สถานะการจ่าย</th>
-      <th width='10%'>ประเภทการจ่าย</th>
-      <th width='10%'>ใบกำกับภาษี</th>
+      <th width='7%'>ผู้บันทึก</th>
+      <th width='10%'>วันที่ทำรายการ</th>
+      <th width='5%'>รับของ</th>
       <th width='10%'>-</th>
     </tr>";
   echo "</thead>";
   $i = 1;
   while ($row = mysqli_fetch_array($result)) {
     echo "<tr>";
-    echo "<td  align=center>" . $i++  . "</td> ";
-    echo "<td>" . "R" . $row["reserve_id"] .  "</td> ";
-    echo "<td> " .date('d-m-Y',strtotime($row["reserve_date"])) . "</td> ";
+    echo "<td align=center>" . $i++  . "</td> ";
+    echo "<td align=center>" . "R" . $row["reserve_id"] .  "</td> ";
+    echo "<td align=center> " .date('d-m-Y',strtotime($row["reserve_date"])) . "</td> ";
     echo "<td>" . $row["customer_name"] . "</td> ";
-    echo "<td>" . $row["receive_status"] . "</td> ";
-    echo "<td>" . $row["payment_status"] . "</td> ";
-    echo "<td>" . $row["payment_type"] . "</td> ";
-    echo "<td align=center><a href='reserve.php?act=vat&ID=$row[reserve_id]' class='btn btn-primary btn-xs'>ใบกำกับภาษี</a></td> ";
+    echo "<td align=center>" . $row["receive_status"] . "</td> ";
+    echo "<td align=center>" . $row["username"] . "</td> ";
+    echo "<td align=center>" . $row["reserve_dt"] . "</td> ";
+    if($row["receive_status"]=='รับของแล้ว'){
+    echo "<td align=center><a href='reserve.php?act=sale&ID=$row[reserve_id]' class='btn btn-primary btn-xs disabled' >รับของ</a></td> ";
     echo "<td align=center><a href='reserve.php?act=view&ID=$row[reserve_id]' class='btn btn-warning btn-xs'>เปิด</a>
-    <a href='reserve.php?act=reserve_cancel&ID=$row[reserve_id]' class='btn btn-danger btn-xs'>คืนสินค้า</a></td> ";
+    <a href='reserve.php?act=reserve_cancel&ID=$row[reserve_id]' class='btn btn-danger btn-xs disabled' >ยกเลิกการจอง</a></td> ";
+    }else{
+    echo "<td align=center><a href='reserve.php?act=sale&ID=$row[reserve_id]' class='btn btn-primary btn-xs'>รับของ</a></td> ";
+    echo "<td align=center><a href='reserve.php?act=view&ID=$row[reserve_id]' class='btn btn-warning btn-xs'>เปิด</a>
+    <a href='reserve.php?act=reserve_cancel&ID=$row[reserve_id]' class='btn btn-danger btn-xs'>ยกเลิกการจอง</a></td> ";
+    }
     echo "</tr>";
   }
   ?>
+  
   </table>
