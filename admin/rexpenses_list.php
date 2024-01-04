53c5a02c469558e4dc827e0e6fa2f0380c8a1bd9
Antace <earthyon@hotmail.com>
2024-01-04 15:11:59 +0700

    Update rexpenses_list.php

--- admin/rexpenses_list.php
+++ admin/rexpenses_list.php
@@ -16,16 +16,17 @@
 
 $query = "SELECT * FROM tb_rexpenses ORDER BY rexpenses_id DESC" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
-echo ' <table id="example1" class="table table-bordered table-striped">';
+echo ' <table id="example1" class="table table-bordered table-hover table-sm">';
   echo "<thead align=center>";
     echo "<tr class='table-light'>
       <th width='5%'>ลำดับ</th>
+      <th width='10%'>เลขที่เบิก</th>
       <th width='15%'>ผู้รับเหมา</th>
       <th width='20%'>ประเภท</th>
       <th width='10%'>จำนวนเงิน</th>
-      <th width='10%'>วันที่ทำรายการ</th>
-      <th width='20%'>สถานะการจ่าย</th>
-      <th width='10%'>-</th>
+      <th width='20%'>วันที่ทำเบิก</th>
+      <th width='10%'>สถานะการจ่าย</th>
+      <th width='10%'></th>
       
     </tr>";
   echo "</thead>";
@@ -33,14 +34,25 @@ echo ' <table id="example1" class="table table-bordered table-striped">';
   while($row = mysqli_fetch_array($result)) {
   echo "<tr>";
     echo "<td  align=center>" .  $i++   .  "</td> ";
-    echo "<td>" .$row["contractor_nickname"] .  "</td> ";
-    echo "<td>" .$row["expenses_name"] .  "</td> ";
-    echo "<td>" .$row["rexpenses_uom"] .  "</td> ";
-    echo "<td>" .$row["rexpenses_dt"] .  "</td> ";
-    echo "<td>" .$row["rexpenses_status"] .  "</td> ";
-    echo "<td align=center><a href='rexpenses.php?act=edit&ID=$row[rexpenses_id]' class='btn btn-warning btn-xs'><i class='fas fa-pencil-alt'></i></a>
-    <a href='rexpenses_del_db.php?ID=$row[rexpenses_id]' onclick=\"return confirm('ยันยันการลบ')\" class='btn btn-danger btn-xs'><i class='fas fa-trash'></i></a>         
-    </td> ";
+    echo "<td align = 'center'>" .$row["rexpenses_id"] .  "</td> ";
+    echo "<td align = 'center'>" .$row["contractor_nickname"] .  "</td> ";
+    echo "<td align = 'center'>" .$row["expenses_name"] .  "</td> ";
+    echo "<td align = 'right'>" .$row["rexpenses_uom"] .  "</td> ";
+    echo "<td align = 'center'>" .$row["rexpenses_date"] .  "</td> ";
+    
+    if($row["rexpenses_status"]==0){
+      echo "<td align=center>" . "<font color = 'red'>"."ยังไม่สั่งจ่าย" ."</font>"."</td> ";
+      }else if($row["rexpenses_status"]>0){
+        echo "<td align=center>" . "<font color = 'green'>". "สั่งจ่ายแล้ว" ."</font>"."</td> ";
+      }
+  
+      if($row["rexpenses_status"]==0){
+      echo "<td align=center><a href='rexpenses.php?act=edit&ID=$row[rexpenses_id]' class='btn btn-warning btn-xs'><i class='fas fa-pencil-alt'></i></a>
+      <a href='rexpenses_del_db.php?ID=$row[rexpenses_id]' onclick=\"return confirm('ยันยันการลบ')\" class='btn btn-danger btn-xs'><i class='fas fa-trash'></i></a></td> ";
+      }else if($row["rexpenses_status"]>0){
+        echo "<td align=center><a href='rexpenses.php?act=edit&ID=$row[rexpenses_id]' class='btn btn-warning btn-xs disabled'><i class='fas fa-pencil-alt'></i></a>
+        <a href='rexpenses_del_db.php?ID=$row[rexpenses_id]' onclick=\"return confirm('ยันยันการลบ')\" class='btn btn-danger btn-xs disabled' ><i class='fas fa-trash'></i></a></td> ";
+      }
   echo "</tr>";
   }
 echo "</table>";
