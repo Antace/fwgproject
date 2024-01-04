dabef1e8c777519f078a1a4eabeea096a6552cc6
Antace <earthyon@hotmail.com>
2024-01-04 15:12:33 +0700

    Update sale_product_list.php

--- admin/sale_product_list.php
+++ admin/sale_product_list.php
@@ -12,21 +12,18 @@
     echo '<meta http-equiv="refresh" content="1;url=sale.php" />';
   }
   $query = "SELECT * FROM tb_sale
-ORDER BY sale_id DESC" or die("Error:" . mysqli_error());
+ORDER BY sale_id DESC" or die("Error:" . mysqli_error($con));
   $result = mysqli_query($con, $query);
 
-  echo ' <table id="example1" class="table table-bordered table-striped">';
+  echo ' <table id="example1" class="table table-bordered table-hover table-sm">';
   echo "<thead  align=center>";
   echo "<tr class=''>
       <th width='3%'>ลำดับ</th>
       <th width='7%'>เลขที่</th>
       <th width='7%'>วันที่ขาย</th>
       <th width='30%'>ชื่อลูกค้า</th>
-      <th width='8%'>จำนวนเงิน</th>
-      <th width='7%'>ภาษี</th>
       <th width='10%'>จำนวนทั้งหมด</th>
-      <th width='6%'>สถานะการจ่าย</th>
-      <th width='10%'>-</th>
+      <th width='10%'></th>
     </tr>";
   echo "</thead>";
   $i = 1;
@@ -36,11 +33,7 @@ ORDER BY sale_id DESC" or die("Error:" . mysqli_error());
     echo "<td>" . "S" . $row["sale_id"] .  "</td> ";
     echo "<td> " .date('d-m-Y',strtotime($row["sale_date"])) . "</td> ";
     echo "<td>" . $row["customer_name"] . "</td> ";
-    echo "<td align=right>" . number_format($row["sale_total"],2) . "</td> ";
-    echo "<td align=right>" . number_format($row["sale_vat"],2) . "</td> ";
     echo "<td align=right>" . number_format ($row["sale_stotal"],2) . "</td> ";
-    echo "<td>" . $row["sale_status"] . "</td> ";
-
     echo "<td align=center><a href='sale.php?act=view&ID=$row[sale_id]' class='btn btn-warning btn-xs'>เปิด</a>
     <a href='sale.php?act=sale_cancel&ID=$row[sale_id]' class='btn btn-danger btn-xs'>คืนสินค้า</a>
     
