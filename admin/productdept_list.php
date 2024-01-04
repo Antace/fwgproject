384ab52ec9d685ee6ec9a822c85a74dd6b892784
Antace <earthyon@hotmail.com>
2024-01-04 15:10:15 +0700

    Update productdept_list.php

--- admin/productdept_list.php
+++ admin/productdept_list.php
@@ -3,24 +3,24 @@ if (@$_GET['do'] == 'success') {
   echo '<script type="text/javascript">
         swal("", "ทำรายการสำเร็จ !!", "success");
         </script>';
-  echo '<meta http-equiv="refresh" content="1;url=product.php" />';
+  echo '<meta http-equiv="refresh" content="1;url=productdept.php" />';
 } else if (@$_GET['do'] == 'finish') {
   echo '<script type="text/javascript">
         swal("", "แก้ไขสำเร็จ !!", "success");
         </script>';
-  echo '<meta http-equiv="refresh" content="1;url=product.php" />';
+  echo '<meta http-equiv="refresh" content="1;url=productdept.php" />';
 }
 $query = "SELECT * FROM tb_productdept
-ORDER BY productdept_id DESC" or die("Error:" . mysqli_error());
+ORDER BY productdept_id DESC" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
-echo ' <table id="example1" class="table table-bordered table-striped">';
+echo ' <table id="example1" class="table table-bordered table-hover table-sm">';
 echo "<thead  align=center>";
 echo "<tr class='table-light'>
       <th width='3%'>ลำดับ</th>
       <th width='30%'>ชื่อสินค้า</th>
       <th width='15%'>ราคาผลิต/ความยาว(เมตร)</th>
 
-      <th width='7%'>-</th>
+      <th width='7%'></th>
     </tr>";
 echo "</thead>";
 $i = 1;
@@ -31,8 +31,8 @@ while ($row = mysqli_fetch_array($result)) {
   echo "<td align=right>" . number_format($row["productdept_price"],2) .  "</td> ";
   
 
-  echo "<td align=center><a href='product.php?act=edit&ID=$row[product_id]' class='btn btn-warning btn-xs'><i class='fas fa-pencil-alt'></i></a>
-  <a href='product_del_db.php?ID=$row[product_id]' onclick=\"return confirm('ยันยันการลบ')\" class='btn btn-danger btn-xs'><i class='fas fa-trash'></i></a>
+  echo "<td align=center><a href='productdept.php?act=edit&ID=$row[productdept_id]' class='btn btn-warning btn-xs'><i class='fas fa-pencil-alt'></i></a>
+  <a href='productdept_del_db.php?ID=$row[productdept_id]' onclick=\"return confirm('ยันยันการลบ')\" class='btn btn-danger btn-xs'><i class='fas fa-trash'></i></a>
 </td> ";
 }
 echo "</table>";
