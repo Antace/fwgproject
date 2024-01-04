f6c53e40ddaf107ea89b4243179ca5e1cdc5bcca
Antace <earthyon@hotmail.com>
2024-01-04 14:42:29 +0700

    Update expenses_list.php

--- admin/expenses_list.php
+++ admin/expenses_list.php
@@ -16,13 +16,13 @@
 
 $query = "SELECT * FROM tb_expenses ORDER BY expenses_id ASC" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
-echo ' <table id="example1" class="table table-bordered table-striped">';
+echo ' <table id="example1" class="table table-bordered table-hover table-sm">';
   echo "<thead align=center>";
     echo "<tr class='table-light'>
       <th width='5%'>ลำดับ</th>
       <th width='65%'>รายการค่าใช้จ่าย</th>
       <th width='20%'>วันที่ทำรายการ</th>
-      <th width='10%'>-</th>
+      <th width='10%'></th>
       
     </tr>";
   echo "</thead>";
@@ -31,7 +31,7 @@ echo ' <table id="example1" class="table table-bordered table-striped">';
   echo "<tr>";
     echo "<td  align=center>" .  $i++   .  "</td> ";
     echo "<td>" .$row["expenses_name"] .  "</td> ";
-    echo "<td>" .$row["expenses_dt"] .  "</td> ";
+    echo "<td align=center>" .$row["expenses_dt"] .  "</td> ";
     echo "<td align=center><a href='expenses.php?act=edit&ID=$row[expenses_id]' class='btn btn-warning btn-xs'><i class='fas fa-pencil-alt'></i></a>
     <a href='expenses_del_db.php?ID=$row[expenses_id]' onclick=\"return confirm('ยันยันการลบ')\" class='btn btn-danger btn-xs'><i class='fas fa-trash'></i></a>         
     </td> ";
