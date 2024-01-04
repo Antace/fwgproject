e7c24f936cf89b9e62c6e760b9575119837255e1
Antace <earthyon@hotmail.com>
2024-01-04 14:39:38 +0700

    Update delivery_product_list.php

--- admin/delivery_product_list.php
+++ admin/delivery_product_list.php
@@ -1,3 +1,21 @@
+<script language="JavaScript">
+	function ClickCheckAll(vol)
+	{
+	
+		var a=1;
+		for(a=1;a<=document.frmdelivery.hdnCount.value;a++)
+		{
+			if(vol.checked == true)
+			{
+				eval("document.frmdelivery.checkbox"+a+".checked=true");
+			}
+			else
+			{
+				eval("document.frmdelivery.checkbox"+a+".checked=false");
+			}
+		}
+	}
+</script>
 <?php
 //connect db
 if(@$_GET['do']=='success'){
@@ -15,31 +33,50 @@ if(@$_GET['do']=='success'){
 $query = "SELECT * FROM tb_delivery
 ORDER BY delivery_id DESC" or die("Error:" . mysqli_error($con));
 $result = mysqli_query($con, $query);
-echo ' <table id="example1" class="table table-bordered table-striped">';
-echo "<thead>";
-  echo "<tr class=''>
+?>
+<form name="frmdelivery" action="deliveryprintall.php" method="post">
+<table id="example1" class="table table-bordered table-hover table-sm">
+<input type = 'submit' class='btn btn-primary btn-sm' name='btnprint' value='พิมพ์รายการที่เลือก'>
+<thead>
+  <tr class=''>
+  <th width='1%'> <div align='center'>
+      <input name='CheckAll' type='checkbox' id='CheckAll'  value='Y' onClick='ClickCheckAll(this);'>
+      </div></th>
     <th width='3%'>ลำดับ</th>
      <th width='10%'>เลขที่ใบส่งของ</th>
      <th width='10%' >วันที่ทำรายการ</th>
-    <th>ชื่อลูกค้า</th>
-    <th>โครงการ</th>
-    <th width='10%'>-</th>
-  </tr>";
-echo "</thead>";
+     <th>ชื่อลูกค้า</th>
+     <th>โครงการ</th>
+     <th width='10%'></th>
+  </tr>
+</thead>
+<?php
 $i = 1;
+$a=0;
 while ($row = mysqli_fetch_array($result)) {
-echo "<tr>";
-  echo "<td  align=center>" . $i++  . "</td> ";
-  echo "<td>" .$row["delivery_id"] .  "</td> ";
-  echo "<td> " .$row["delivery_date"] . "</td> ";
-  echo "<td>" .$row["customer_name"] ."</td> ";
-  echo "<td>" .$row["department_name"] ."</td> ";
+  $a++;
+  ?>
+<tr>
+  <td align=center><input type='checkbox'  name='checkbox[]' id='checkbox<?php echo $a;?>' value='<?php echo $row['delivery_id'];?>'></td>
+  <td  align=center> <?php echo $i++;?> </td>
+  <td> <?php echo $row["delivery_id"];?> </td>
+  <td> <?php echo $row["delivery_date"];?> </td>
+  <td> <?php echo $row["customer_name"];?> </td>
+  <td> <?php echo $row["department_name"];?> </td>
   
-  echo "<td align=center><a href='delivery.php?act=view&ID=$row[delivery_id]' class='btn btn-warning btn-xs'>เปิด</a>
-  <a href='delivery.php?act=delivery_cancel&ID=$row[delivery_id]' class='btn btn-danger btn-xs'>คืนสินค้า</a>
-  
-</td> ";
-  echo "</tr>";
+  <td align=center>
+    <a href='delivery.php?act=print&ID=<?php echo $row['delivery_id'];?>' class='btn btn-primary btn-xs'>พิมพ์</a>
+    <a href='delivery.php?act=view&ID=<?php echo $row['delivery_id'];?>' class='btn btn-warning btn-xs'>เปิด</a>
+    <a href='delivery.php?act=delivery_cancel&ID=<?php echo $row['delivery_id'];?>' class='btn btn-danger btn-xs'>คืนสินค้า</a> 
+  </td> 
+</tr>
+<?php
 }
 ?>
 </table>
+<?php
+mysqli_close($con);
+?>
+
+<input type='hidden' name='hdnCount' value='<?php echo $a; ?>'>
+</form>
