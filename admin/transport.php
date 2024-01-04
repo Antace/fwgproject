088c53c290c746b199163026cfe8b6c1ddd03749
Antace <earthyon@hotmail.com>
2024-01-04 15:13:21 +0700

    Create transport.php

---
+++ admin/transport.php
@@ -0,0 +1,60 @@
+<?php include('h.php');?>
+<body class="hold-transition skin-purple sidebar-mini">
+  <div class="wrapper">
+    <!-- Main Header -->
+    <?php include('menutop.php');?>
+        <?php include('menu_l.php');?>
+    <div class="content-wrapper">
+      <section class="content-header">
+        <h1>
+        <i class="glyphicon glyphicon-check hidden-xs"></i> <span class="hidden-xs">ใบส่งของ</span> 
+        <a href="transport_form_add1.php" class="btn btn-primary btn-sm">เพิ่ม</a>
+        
+        </h1>
+      </section>
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="sticky-top mb-12">
+                <div class="card">
+                  <div class="card-body">
+                    <?php
+                     $act = (isset($_GET['act']) ? $_GET['act'] : '');
+                     if($act == 'view'){
+                     include('transport_form_view.php');
+                     }elseif($act == 'transport_cancel'){
+                      include('transport_form_view.php');
+                     }elseif ($act == 'print') {
+                      include('transport_form_print.php');
+                    }else {
+                     include('transport_product_list.php');  
+                     
+                     }
+                       
+                  ?>         
+                 <!-- /.card-body -->
+                  </div>
+                  <!-- /.card -->
+                </div>
+                <!-- /.col -->
+                
+              </div>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+          
+        </div>
+      </div>
+      
+    </div>
+    <!-- /.row -->
+    </div><!-- /.container-fluid -->
+      </section>
+    <?php include ('footer.php');?>
+  </html>
+  <?php include('footerjs.php');?>
+
+
+
