b83afd511daaa3055e1bb160c0dabf27a0860c3b
Antace <earthyon@hotmail.com>
2024-01-04 15:09:19 +0700

    Update po_list_show42.php

--- admin/po_list_show42.php
+++ admin/po_list_show42.php
@@ -2,7 +2,7 @@
 include "po_list_show_h.php";
   
   $query = "SELECT * FROM tb_po 
-  WHERE department_name LIKE '%$qdept%' "  or die("Error:" . mysqli_error());
+  WHERE department_name LIKE '%$qdept%' "  or die("Error:" . mysqli_error($con));
   $result = mysqli_query($con, $query);
   include "po_slist.php";
 
