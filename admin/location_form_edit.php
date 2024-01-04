7f73cd31ef3579c09555e1a623a08cbdd01f28bd
Antace <earthyon@hotmail.com>
2024-01-04 14:43:38 +0700

    Create location_form_edit.php

---
+++ admin/location_form_edit.php
@@ -0,0 +1,44 @@
+<?php 
+
+$ID = mysqli_real_escape_string($con,$_GET['ID']);
+$sql = "SELECT * FROM tb_location WHERE location_id=$ID";
+$result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error($con));
+$row = mysqli_fetch_array($result);
+?>
+<div align="right">
+  <font color="red">*</font>
+  <font color="gray">Required Fields</font>
+</div>
+<hr>
+<form action="location_form_edit_db.php" method="post" class="form-horizontal">
+  <div class="form-group">
+    <div class="col-sm-3 control-label">
+      ชื่อสถานที่จัดเก็บ : <font color="red">*</font>
+    </div>
+    <div class="col-sm-9">
+      <input type="text" name="location_name" required class="form-control" value="<?php echo $row['location_name'];?>">
+    </div>
+  </div>
+  <div class="form-group">
+      <div class="col-sm-6">
+        <input type="hidden" name="username" required class="form-control" value="<?php echo $username; ?>" readonly >
+      </div>
+    </div>
+  <hr>
+
+  <div class="form-group">
+    <div class="col-sm-12">
+     <font color ="gray">บันทึกข้อมูลล่าสุด : <?php echo $row['location_dt']; ?> ผู้บันทึก : <?php echo $row ['username']; ?> </font> 
+    </div>
+  </div>
+  <hr>
+    <div class="form-group">
+    <div class="col-sm-3">
+    </div>
+    <div class="col-sm-3">
+      <input type="hidden" name="location_id" value="<?php echo $ID; ?>" />
+      <button type="submit" class="btn btn-success">บันทึก</button>
+      <a href="location.php" class="btn btn-danger">ยกเลิก</a>
+    </div>
+  </div>
+</form>
