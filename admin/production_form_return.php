28960ac5456dd5553064c11d126d01d5664e6d35
Antace <earthyon@hotmail.com>
2024-01-04 15:10:24 +0700

    Update production_form_return.php

--- admin/production_form_return.php
+++ admin/production_form_return.php
@@ -25,7 +25,7 @@ $production_dt = Date( "Y-m-d G:i:s" );
 mysqli_query( $con, "BEGIN" );
 
 $sql1 = "DELETE FROM tb_production WHERE production_id=$production_id";
-$query1 = mysqli_query($con,$sql1) or die ("Error in query: $sql1" . mysqli_error($sql1));
+$query1 = mysqli_query($con,$sql1) or die ("Error in query: $sql1" . mysqli_error($con,$sql1));
 
 echo '<br>';
 echo 'sql1 ='. $sql1;
@@ -33,7 +33,7 @@ echo 'sql1 ='. $sql1;
 // ฟังก์ชั่น MAX() จะคืนค่าที่มากที่สุดในคอลัมน์ที่ระบุ ออกมา หรือจะพูดง่ายๆก็ว่า ใช้สำหรับหาค่าที่มากที่สุด นั่นเอง.
 for($a=0;$a<count($_POST["productionlist_id"]);$a++){
 $sql2 = "DELETE FROM tb_productionlist WHERE productionlist_id = '".$_POST["productionlist_id"][$a]."'";
-$query2 = mysqli_query( $con, $sql2 )or die( "Error in query: $sql2" . mysqli_error( $sql2 ) );
+$query2 = mysqli_query( $con, $sql2 )or die( "Error in query: $sql2" . mysqli_error($con, $sql2 ) );
 
 }
 // $row2 = mysqli_fetch_array( $query2 );
@@ -44,7 +44,7 @@ $query2 = mysqli_query( $con, $sql2 )or die( "Error in query: $sql2" . mysqli_er
 // $string = implode(",",$_POST["checkbox"]);
 for($i=0;$i<count($_POST["product_id"]);$i++){
   $sql3 = "SELECT * FROM tb_product WHERE product_id = '".$_POST["product_id"][$i]."'";
-  $query3 = mysqli_query( $con, $sql3 )or die( "Error in query: $sql3" . mysqli_error( $sql3 ) );
+  $query3 = mysqli_query( $con, $sql3 )or die( "Error in query: $sql3" . mysqli_error( $con,$sql3 ) );
   $row3 = mysqli_fetch_array( $query3 );
   
   $count=mysqli_num_rows($query3);
@@ -56,22 +56,22 @@ echo '<br>';
 // echo '$_POST[production_uom]='.$_POST['production_uom'][$i];
   //ตัดสต๊อก
 
-    // $instock = $row3[ 'product_uom' ];
-    // $production_uom = $_POST['production_uom'][$i];
+    $instock = $row3[ 'product_uom' ];
+    $production_uom = $_POST['production_uom'][$i];
 
-    // $stc = $instock - $production_uom; //เอาจำนวนสินค้าที่มีอยู่มาบวกด้วยจำนวนคืนสินค้า
-    // echo '<br>';
-    // echo 'instcok ='.$instock;
-    // echo '<br>';
-    // echo 'stc ='.$stc;
-    // echo '<br>';
-    // echo 'production_uom ='.$production_uom;
-    // echo '<br>';
+    $stc = $instock - $production_uom; //เอาจำนวนสินค้าที่มีอยู่มาบวกด้วยจำนวนคืนสินค้า
+    echo '<br>';
+    echo 'instcok ='.$instock;
+    echo '<br>';
+    echo 'stc ='.$stc;
+    echo '<br>';
+    echo 'production_uom ='.$production_uom;
+    echo '<br>';
    
-    // $sql5 = "UPDATE tb_product SET  
-    //  product_uom=$stc
-    //  WHERE  product_id='".$_POST["product_id"][$i]."'";
-    // $query5 = mysqli_query( $con, $sql5 )or die( "Error in query: $sql5" . mysqli_error( $sql5 ) );
+    $sql5 = "UPDATE tb_product SET  
+     product_uom=$stc
+     WHERE  product_id='".$_POST["product_id"][$i]."'";
+    $query5 = mysqli_query( $con, $sql5 )or die( "Error in query: $sql5" . mysqli_error( $con,$sql5 ) );
 
 
     // echo 'sql5 ='.$sql5;
@@ -79,7 +79,7 @@ echo '<br>';
   
 
 }
-exit;
+// exit;
 
 if ( $query1 && $query3 ) {
   mysqli_query( $con, "COMMIT" );
