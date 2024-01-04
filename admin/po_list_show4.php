a981af8efe46511f46435241137d37862ca2f0b1
Antace <earthyon@hotmail.com>
2024-01-04 15:09:15 +0700

    Update po_list_show4.php

--- admin/po_list_show4.php
+++ admin/po_list_show4.php
@@ -2,7 +2,7 @@
 include "po_list_show_h.php";
   
   $query = "SELECT * FROM tb_po 
-  WHERE po_name LIKE '%$qpo%'  "  or die("Error:" . mysqli_error());
+  WHERE po_name LIKE '%$qpo%'  "  or die("Error:" . mysqli_error($con));
   $result = mysqli_query($con, $query);
   include "po_slist.php";
 
