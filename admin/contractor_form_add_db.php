38cc010b42a4f27b957d5bd4ca984d6db8ade559
Antace <earthyon@hotmail.com>
2024-01-04 14:39:07 +0700

    Update contractor_form_add_db.php

--- admin/contractor_form_add_db.php
+++ admin/contractor_form_add_db.php
@@ -12,9 +12,27 @@ if($_SESSION['employee_id']==''){
 	$contractor_name = mysqli_real_escape_string($con,$_POST["contractor_name"]);
 	$contractor_nickname = mysqli_real_escape_string($con,$_POST["contractor_nickname"]);
 	$contractor_nid = mysqli_real_escape_string($con,$_POST["contractor_nid"]);
+	$contractor_file = mysqli_real_escape_string($con,$_POST["contractor_file"]);
+	$contractor_expired = mysqli_real_escape_string($con,$_POST["contractor_expired"]);
+	$contractor_address = mysqli_real_escape_string($con,$_POST["contractor_address"]);
+	$contractor_bank = mysqli_real_escape_string($con,$_POST["contractor_bank"]);
+	$account_number = mysqli_real_escape_string($con,$_POST["account_number"]);
 	$username = mysqli_real_escape_string($con,$_POST["username"]);
 
-	
+	$date1 = date("Ymd_His");
+	$numrand = (mt_rand());
+	$prodcut_img = (isset($_POST['contractor_file']) ? $_POST['contractor_file'] : '');
+	$upload=$_FILES['contractor_file']['name'];
+	$type = strrchr($_FILES['contractor_file']['name'],".");
+
+	if($upload !='') { 
+		$path="../contractor_file/";
+		$type = strrchr($_FILES['contractor_file']['name'],".");
+		$fileNewName =$numrand.$date1.$type;
+		$path_copy=$path.$fileNewName;
+		$path_link="../contractor_file/".$fileNewName;
+		move_uploaded_file($_FILES['contractor_file']['tmp_name'],$path_copy); 
+		}
 	
 	
 
@@ -38,6 +56,11 @@ if($_SESSION['employee_id']==''){
 	contractor_name,
 	contractor_nickname,
 	contractor_nid,
+	contractor_file,
+	contractor_expired,
+	contractor_address,
+	contractor_bank,
+	account_number,
 	username
 	)
 	VALUES
@@ -45,6 +68,11 @@ if($_SESSION['employee_id']==''){
 	'$contractor_name',
 	'$contractor_nickname',
 	'$contractor_nid',
+	'$fileNewName',
+	'$contractor_expired',
+	'$contractor_address',
+	'$contractor_bank',
+	'$account_number',
 	'$username'
 	)";
 
